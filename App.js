7aa7c99953cf98bef5c2ed01d9c93dcf6bdd5f0a
ShuVeriDa <bashtarov@outlook.com>
2023-08-15 21:32:01 +0300

    added ability to request posts and loader when requesting data

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {ActivityIndicator, Alert, FlatList, StatusBar, Text, View} from 'react-native';
+import {Post} from "./components/Post";
+import {useEffect, useState} from "react";
+import axios from "axios";
+
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [articles, setArticles] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true)
+      try {
+        const {data} = await axios.get('https://630a32f93249910032824d12.mockapi.io/drinks')
+        setArticles(data)
+        setIsLoading(false)
+      } catch (e) {
+        setIsLoading(false)
+        console.log(e)
+        Alert.alert("Ошибка", "Не удалось получить статьи")
+      }
+    }
+
+    fetchData()
+  }, [])
+
+  if (isLoading) {
+    return (
+      <View style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+
+        <ActivityIndicator/>
+        <Text style={{marginTop: "15px"}}>Загрузка...</Text>
+      </View>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <FlatList data={articles}
+                renderItem={({item}) => <Post
+                  title={item.title}
+                  imageUrl={item.imageUrl}
+                  createdAt={item.createdAt}
+                />}
+      />
+      <StatusBar/>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
